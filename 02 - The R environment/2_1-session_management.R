c85b45fb5dfe012fb089651dd3c0ab216ad459fc
FranFT <frannnlol@gmail.com>
2025-07-13 10:12:02 +0200

    Executing other R files from a script using data() function.

--- 02 - The R environment/2_1-session_management.R
+++ 02 - The R environment/2_1-session_management.R
@@ -13,4 +13,33 @@
   rm(list=ls())
   ls()
   
+  # You can save the workspace into a file with the command "save.image()"
+  # (creates a .RData file in rood directory)
+  save.image()
+  # Use "load()" to load save files into your current workspace.
+  
+  # You can get the current work directory with:
+  getwd()
+  # And set a new working directory with "setwd(mydir)"
+  
+  # To load packages you can use the command
+  library(ISwR)
+  # To unload packages you can use the command:
+  detach("package:ISwR")
+
+#####################
+### Built-in data ###
+#####################
+  # To be able to use a built-in data set you need to load the data frame first.
+  # This can be done with the function "data()"
+  data("thuesen")
+  
+  # data() will look for files whose base name matches the given name. It also
+  # searches in the sub-directory "data" if it exists. Files with .tab extension
+  # are read using read.table and .R file are executed as source files.
+  # load_data_test.R is located under "/data" folder. Inside, the variable
+  # "my_variable" is created. After using "data()" function, we can reference
+  # it in this script.
+  data("load_data_test")
+  my_variable
   
---
+++ data/load_data_test.R
@@ -0,0 +1,2 @@
+# This is a test
+my_variable <- "This is a test for the data() function."
